1e3d13a6c48b6ffea315267a7405a4d5e845467e
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-11-22 07:23:41 +0000

    os: 'build.sh' provided with 'I12E_DEBUG=1' support

--- os/build.sh
+++ os/build.sh
@@ -6,14 +6,21 @@ cd "$(dirname "$0")"
 
 [ "$I12E_ENV" != "prod" ] && I12E_ENV="dev"
 
-HELM="helm template $I12E_ENV ."
+HELM="helm template"
+[ "$I12E_DEBUG" = "1" ] && HELM="${HELM} --debug"
+HELM="${HELM} $I12E_ENV ."
 
 for FNAME in secrets.yaml ../secrets.yaml
 do
   [ -f "$FNAME" ] && HELM="${HELM} -f secrets://${FNAME}"
 done
 
+if [ "$I12E_DEBUG" = "1" ]
+then
+  set -x
+  exec $HELM
+fi
+
 set -x
 $HELM | docker run --rm -i quay.io/coreos/butane:latest > os.json
-
 ls -l os.json
