2e2863374ebd6f58683f01f2f779a845a4b2d267
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-07 13:48:27 +0000

    spark: '_spark.py' provided with 'update_conf()' function

--- spark/templates/_spark.py
+++ spark/templates/_spark.py
@@ -8,8 +8,6 @@ from logging import getLogger, basicConfig, INFO
 basicConfig(format='%(asctime)s [%(relativeCreated)7.0f] [%(levelname).1s] %(message)s',level=INFO,stream=stderr)
 log = getLogger(__name__)
 SPARK_BASE = "/spark"
-UPDATE_CONF = f"{SPARK_BASE}/etc/flatcar/update.conf"
-UPDATE_CONF_BUF = """{{ include "spark.etc.flatcar.update.conf" . }}"""
 SPARK_YAML = f"{SPARK_BASE}/var/lib/rancher/k3s/server/manifests/spark.yaml"
 SPARK_SKIP = f"{SPARK_YAML}.skip"
 CONFIG_YAML = f"{SPARK_BASE}/etc/rancher/k3s/config.yaml"
@@ -26,6 +24,16 @@ cluster-init: true
 node-ip: "192.168.56.51"
 flannel-iface: "enp0s8"
 """
+def update_conf():
+    fname = f"{SPARK_BASE}/etc/flatcar/update.conf"
+    if not isfile(fname):
+        log.warning("skipping '{0}': it's not a regular file".format(fname))
+        return
+    buf = """{{ include "spark.etc.flatcar.update.conf" . }}"""
+    with open(fname,"w") as fp:
+        fp.write(buf.strip() + "\n")
+        fchmod(fp.fileno(),0o644)
+    log.info("'{0}' updated".format(fname))
 def main():
     log.info("==== spark begin ====")
     # https://docs.k3s.io/installation/packaged-components
@@ -49,13 +57,7 @@ def main():
             log.info(" (after) {0}: {1}".format(fname,readlink(fname)))
         except FileNotFoundError as e:
             log.warning("skipping '{0}': {1}".format(fname,str(e)))
-    if isfile(UPDATE_CONF):
-        with open(UPDATE_CONF,"w") as fp:
-            fp.write(UPDATE_CONF_BUF.strip() + "\n")
-            fchmod(fp.fileno(),0o644)
-        log.info("'{0}' updated".format(UPDATE_CONF))
-    else:
-        log.warning("skipping '{0}': it's not a regular file".format(UPDATE_CONF))
+    update_conf()
     log.info("---- spark end ----")
     # #chroot /spark systemd-run bash -c 'sleep 1 ; systemctl reboot'
     # # Failed to connect to system scope bus via local transport: No data available
