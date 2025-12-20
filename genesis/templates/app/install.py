8013c643b4ee2466a9bedb57fa9c993193bed49b
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-20 17:21:48 +0000

    genesis: local butane generation proof-of-concept created

--- genesis/templates/app/install.py
+++ genesis/templates/app/install.py
@@ -3,9 +3,9 @@
 #!/usr/bin/env python3
 from os import chmod,fchmod,readlink,symlink,unlink,mkdir,getenv
 from os.path import islink,isfile,isdir
-from subprocess import call
+from subprocess import call,Popen,PIPE
+import yaml,json
 from logging import getLogger
-
 log = getLogger(__name__)
 
 class GenesisInstall(object):
@@ -82,13 +82,47 @@ class GenesisInstall(object):
         # chroot "${GENESIS_BASE}" systemctl reboot
         call(["chroot",self.__base,"systemctl","reboot"])
 
+    def __butane(self):
+        buf_yaml = """variant: flatcar
+version: 1.0.0
+{{ include "genesis.flatcar.passwd.users" ( dict "ssh_authorized_key" (first .Values.env.dev.ssh_authorized_keys) ) }}
+"""
+        _data = yaml.safe_load(buf_yaml)  # check it is valid
+        cmd = ['butane']
+        p = Popen(args=cmd,stdin=PIPE,stdout=PIPE,stderr=PIPE)
+        (odata,edata) = p.communicate(buf_yaml.encode())
+        if p.returncode != 0:
+            raise Exception("'{0}' command failed: {1}".format(" ".join(cmd),edata.decode().strip()))
+        config_ign_new = odata.decode().strip()
+        js_new = json.loads(config_ign_new)
+        fname = "{0}/oem/config.ign".format(self.__base)
+        with open(fname,"r") as fp:
+            buf = fp.read()
+        js_old = json.loads(buf)
+        if js_new == js_old:
+            log.info("nothing to do: butane config did not change")
+            return
+        log.info("upgrading flatcar...")
+        fname = "{0}/root/config.ign".format(self.__base)
+        with open(fname,"w") as fp:
+            fp.write(config_ign_new)
+        cmds = [
+            ["chroot",self.__base,"flatcar-reset","--keep-machine-id","--keep-paths","/etc/ssh/ssh_host_.*","/var/log","-F","/root/config.ign"],
+            ["chroot",self.__base,"systemd-run","bash","-c","sleep 1 ; systemctl reboot"],
+        ]
+        for cmd in cmds:
+            ret = call(cmd)
+            if ret != 0:
+                raise Exception("'{0}' command failed: ret={1}".format(" ".join(cmd),ret))
+
     def run(self):
         log.info("==== genesis install begin ====")
         #self.__flatcar_extensions()
-        self.__flatcar_update_conf()
+        #self.__flatcar_update_conf()
         #self.__k3s_config_yaml()
         #self.__k3s_override_conf()
         #self.__manifest_skip()
         #self.__reboot()
+        self.__butane()
         log.info("---- genesis install end ----")  # never reached
 {{ end }}
