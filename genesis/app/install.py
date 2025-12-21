ab9f3b51f86dce39eda3ca4131cde0e9f500a16a
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-21 10:47:34 +0000

    genesis: some 'butane.py' code moved back to 'install.py'

--- genesis/app/install.py
+++ genesis/app/install.py
@@ -10,6 +10,70 @@ class GenesisInstall(object):
     def __init__(self):
         self.__base = "/genesis"
 
+    # def __flatcar_extensions(self):
+    #     for entry in ["containerd","docker"]:
+    #         fname = "{0}/etc/extensions/{1}-flatcar.raw".format(self.__base,entry)
+    #         try:
+    #             if not islink(fname):
+    #                 log.warning("skipping '{0}': it's not a symlink".format(fname))
+    #                 continue
+    #             log.info("(bef) {0}: {1}".format(fname,readlink(fname)))
+    #             unlink(fname)
+    #             symlink("/dev/null",fname)
+    #             log.info("(aft) {0}: {1}".format(fname,readlink(fname)))
+    #         except FileNotFoundError as e:
+    #             log.warning("skipping '{0}': {1}".format(fname,str(e)))
+
+    # def __flatcar_update_conf(self):
+    #     fname = "{0}/etc/flatcar/update.conf".format(self.__base)
+    #     if not isfile(fname):
+    #         log.warning("skipping '{0}': it's not a regular file".format(fname))
+    #         return
+    #     with open(fname,"r") as fp:
+    #         buf_old = fp.read()
+    #     buf_new = """{{ include "genesis.flatcar.update.conf" . }}"""
+    #     if buf_old == buf_new:
+    #         log.info("nothing to do: '{0}' is up-to-date".format(fname))
+    #         return
+    #     with open(fname,"w") as fp:
+    #         fp.write(buf_new)
+    #         fchmod(fp.fileno(),0o644)
+    #     cmd = ["chroot",self.__base,"systemctl","restart","update-engine"]
+    #     ret = call(cmd)
+    #     if ret != 0:
+    #         raise Exception("'{0}' command failed: ret={1}".format(" ".join(cmd),ret))
+    #     log.info("'{0}' updated and update-engine restarted".format(fname))
+
+    # def __k3s_config_yaml(self):
+    #     fname = "{0}/etc/rancher/k3s/config.yaml".format(self.__base)
+    #     buf = """{{ include "genesis.k3s.config.yaml" . }}"""
+    #     with open(fname,"w") as fp:
+    #         fp.write(buf.strip() + "\n")
+    #         fchmod(fp.fileno(),0o600)
+    #     log.info("'{0}' created".format(fname))
+
+    # def __k3s_override_conf(self):
+    #     dname = "{0}/etc/systemd/system/k3s.service.d".format(self.__base)
+    #     fname = "{0}/override.conf".format(dname)
+    #     buf = """{{ include "genesis.k3s.override.conf" . }}"""
+    #     if not isdir(dname):
+    #         mkdir(dname)
+    #     if not isdir(dname):
+    #         raise Exception("error: couldn't create '{0}' folder".format(dname))
+    #     chmod(dname,0o755)
+    #     with open(fname,"w") as fp:
+    #         fp.write(buf.strip() + "\n")
+    #         fchmod(fp.fileno(),0o644)
+    #     log.info("'{0}' created".format(fname))
+
+    # def __manifest_skip(self):
+    #     # https://docs.k3s.io/installation/packaged-components
+    #     # don't let genesis.yaml run on k3s(etcd)
+    #     fname = "{0}/var/lib/rancher/k3s/server/manifests/genesis.yaml.skip".format(self.__base)
+    #     with open(fname,"w") as fp:
+    #         fchmod(fp.fileno(),0o600)
+    #     log.info("'{0}' created".format(fname))
+
     def __butane(self):
         fname = "/sec/ssh_authorized_key"
         with open(fname,"r") as fp:
@@ -64,6 +128,6 @@ class GenesisInstall(object):
 
     def run(self):
         log.info("==== genesis install begin ====")
-        self.__butane()
-        self.__reboot()
+        #self.__butane()
+        #self.__reboot()
         log.info("---- genesis install end ----")
