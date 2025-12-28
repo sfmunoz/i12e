83f1cc5685ef41ab18020225085d629950e63cbf
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-28 17:33:55 +0000

    install.sh: version bumped to 'v0.0.3'

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # curl -sfL https://raw.githubusercontent.com/sfmunoz/i12e/refs/tags/${VERSION}/install.sh | sh -
-VERSION="v0.0.2"
+VERSION="v0.0.3"
 SHA256="3ec7822baf91b0a4a7dac3c58b3e3f8fbff9d5bd61943e461b89df85cb110fb4"
 set -e -o pipefail
 TFILE="$(mktemp)"
