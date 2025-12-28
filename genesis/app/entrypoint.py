5c7eb8b341e53d410ef28f381448800016ba96c4
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-28 10:17:06 +0000

    genesis: 'entrypoint.py' runs 'python3 -m genesis'

--- genesis/app/entrypoint.py
+++ genesis/app/entrypoint.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from base64 import b64decode
 from pathlib import Path
+from os import execlp
 blob_file = Path(__file__).resolve().parent / "code.blob"
 ofolder = Path("/app")
 for line in blob_file.read_text().strip().split("\n"):
@@ -8,3 +9,4 @@ for line in blob_file.read_text().strip().split("\n"):
     p = ofolder / ofile
     p.parent.mkdir(parents=True,exist_ok=True)
     p.write_text(buf)
+execlp("python3","python3","-m","genesis")
