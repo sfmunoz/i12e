35ccfd991f050bef6e989aeca6b1eade41bd8a32
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-28 09:36:05 +0000

    genesis: 'entrypoint.py' simplified

--- genesis/app/entrypoint.py
+++ genesis/app/entrypoint.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-from sys import argv
 from base64 import b64decode
 from pathlib import Path
-from os.path import join
-blob_file,ofolder = argv[1],argv[2]
-with open(blob_file,"r") as fp:
-    buf = fp.read().strip()
-for i,line in enumerate(buf.split("\n")):
+blob_file = Path(__file__).resolve().parent / "code.blob"
+ofolder = Path("/app")
+for line in blob_file.read_text().strip().split("\n"):
     ofile,buf = [b64decode(x).decode() for x in line.split("|")]
-    p = Path(join(ofolder,ofile))
+    p = ofolder / ofile
     p.parent.mkdir(parents=True,exist_ok=True)
-    with open(p,"w") as fp:
-        fp.write(buf)
+    p.write_text(buf)
