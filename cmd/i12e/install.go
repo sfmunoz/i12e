18e0e34702740d88a430483428e2186bd23749dd
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-28 18:19:02 +0000

    i12e: 'i12e_service_enable()' support created

--- cmd/i12e/install.go
+++ cmd/i12e/install.go
@@ -1,8 +1,14 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"strings"
+)
 
-const i12e_service = `[Unit]
+func i12e_service_install() {
+	const i12e_service = `[Unit]
 Description=i12e
 Wants=network-online.target
 After=network-online.target
@@ -16,12 +22,34 @@ Restart=always
 RestartSec=5s
 ExecStart=/opt/bin/i12e
 `
-
-func install() {
-	log.Info("installing...")
 	fname := "/etc/systemd/system/i12e.service"
 	err := os.WriteFile(fname, []byte(i12e_service), 0644)
 	if err != nil {
-		log.Fatal("os.WriteFile() failed", "fname", fname, "err", err)
+		log.Fatal("error: 'os.WriteFile()' failed", "fname", fname, "err", err)
 	}
+	log.Info("i12e_service_install() complete", "fname", fname)
+}
+
+func i12e_service_enable() {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("systemctl", "enable", "i12e.service")
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal("error: 'cmd.Run()' failed", "err", err, "stderr", stderr.String())
+	}
+	for line := range strings.SplitSeq(stdout.String(), "\n") {
+		if line == "" {
+			continue
+		}
+		log.Info(line)
+	}
+	log.Info("i12e_service_enable() complete")
+}
+
+func install() {
+	log.Info("installing...")
+	i12e_service_install()
+	i12e_service_enable()
 }
