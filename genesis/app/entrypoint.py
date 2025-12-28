044ccae1496cff08cdcad2f963a47cd005dac707
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-28 07:56:19 +0000

    genesis: 'entrypoint.py' supports blob extraction

--- genesis/app/entrypoint.py
+++ genesis/app/entrypoint.py
@@ -1,7 +1,17 @@
-import sys
-if __name__ == "__main__":
-    with open(sys.argv[1],"r") as fp:
-        buf = fp.read().strip()
-    for i,line in enumerate(buf.split("\n")):
-        print(i,line)
-
+#!/usr/bin/env python3
+from sys import argv
+from base64 import b64decode
+from pathlib import Path
+from os.path import join
+blob_file,ofolder = argv[1],argv[2]
+with open(blob_file,"r") as fp:
+    buf = fp.read().strip()
+ofile = ""
+for i,line in enumerate(buf.split("\n")):
+    entry = b64decode(line).decode()
+    if i % 2 == 0:
+        ofile = join(ofolder,entry)
+        continue
+    Path(ofile).parent.mkdir(parents=True,exist_ok=True)
+    with open(ofile,"w") as fp:
+        fp.write(entry)
