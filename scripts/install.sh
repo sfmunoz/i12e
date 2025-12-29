8155cd8790fd0dc11a3e1c7cfdfb2e7ebc0e18fa
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-12-29 07:41:28 +0000

    i12e: 'install.sh' moved to 'scripts/install.sh'

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# curl -sfL https://raw.githubusercontent.com/sfmunoz/i12e/refs/tags/${VERSION}/install.sh | sh -
+# curl -sfL https://raw.githubusercontent.com/sfmunoz/i12e/refs/tags/${VERSION}/scripts/install.sh | sh -
 VERSION="v0.0.3"
 SHA256="3ec7822baf91b0a4a7dac3c58b3e3f8fbff9d5bd61943e461b89df85cb110fb4"
 set -e -o pipefail
