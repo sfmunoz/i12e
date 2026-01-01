5d19c1a90c0250059d3ea5abcb83ccba3e88aca6
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-01 19:04:34 +0000

    Makefile: 'build/i12e' binary is built instead of 'bin/i12e'

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-all: bin/i12e
+all: build/i12e
 
-bin/i12e: ./cmd/i12e/main.go ./internal/install/i12e_install.go
-	go build -trimpath -buildvcs=false -ldflags="-s -w" -o bin/i12e ./cmd/i12e
+build/i12e: ./cmd/i12e/main.go ./internal/install/i12e_install.go
+	go build -trimpath -buildvcs=false -ldflags="-s -w" -o build/i12e ./cmd/i12e
 
 clean:
-	rm -rf bin
+	rm -rf build
 
 .PHONY: all clean
--- scripts/i12e-ext.sh
+++ scripts/i12e-ext.sh
@@ -39,12 +39,11 @@ D="build/i12e-flatcar"
 set -e -o pipefail
 cd "$(dirname "$0")/.."
 set -x
-rm -rf build
 make clean
 umask 022
 mkdir -p $D/usr/bin $D/usr/lib/extension-release.d $D/usr/lib64
 make
-cp bin/i12e $D/usr/bin/i12e
+cp build/i12e $D/usr/bin/i12e
 cp /usr/bin/{tmux,rclone} $D/usr/bin
 cp -a /usr/lib/x86_64-linux-gnu/libutempter.so* $D/usr/lib64
 cat << __EOF > $D/usr/lib/extension-release.d/extension-release.i12e-flatcar
