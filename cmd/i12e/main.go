b05c39dbf9c93a3fc6f4dbf8e639526db8e8eb6a
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-09 17:44:34 +0000

    i12e: 'ip4addrs()' method created to get IP4 addresses from given interface

--- cmd/i12e/main.go
+++ cmd/i12e/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"os"
 	"time"
 
@@ -14,6 +16,33 @@ var log = logit.Logit().
 	With("mod", "i12e").
 	With("pkg", "main")
 
+func ip4addrs(iname string) ([]string, error) {
+	iface, err := net.InterfaceByName(iname)
+	if err != nil {
+		return nil, fmt.Errorf("net.InterfaceByName() failed: %s", err.Error())
+	}
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, fmt.Errorf("iface.Addrs() failed: %s", err.Error())
+	}
+	ret := make([]string, 0)
+	for _, addr := range addrs {
+		ipNet, ok := addr.(*net.IPNet)
+		if !ok {
+			continue
+		}
+		ip := ipNet.IP
+		if ip.To4() == nil {
+			continue
+		}
+		ret = append(ret, ip.String())
+	}
+	if len(ret) < 1 {
+		return nil, fmt.Errorf("no IPv4 address found")
+	}
+	return ret, nil
+}
+
 func main() {
 	if os.Getenv("I12E_INSTALL") == "1" {
 		install.I12eInstall()
@@ -25,6 +54,12 @@ func main() {
 		install.K3sInstall()
 		pull.Pull()
 		log.Info("i12e sleeping...", "slumber", slumber)
+		addr, err := ip4addrs("enp0s8")
+		if err != nil {
+			log.Error("ip4addrs() failed", "err", err)
+		} else {
+			log.Info("ip4addrs()", "addr", addr[0])
+		}
 		time.Sleep(slumber)
 	}
 }
