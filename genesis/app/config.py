96666f4e967460d5fc54aebf2d61c955e48e83f3
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-09 16:49:16 +0000

    i12e: 'secrets-dev.yaml/secrets-prod.yaml' used instead of 'secrets.yaml' with 'dev/prod' configs

--- genesis/app/config.py
+++ genesis/app/config.py
@@ -9,18 +9,13 @@ log = getLogger(__name__)
 
 class Config(object):
     def __init__(self):
-        self.__env = getenv("I12E_ENV")
-        if self.__env is None:
-            raise Exception("undefined 'I12E_ENV' env-var")
-        if self.__env not in ["dev","prod"]:
-            raise Exception("unknown I12E_ENV='{0}' value (valid: 'dev' or 'prod')".format(self.__env))
-
-    def secrets_yaml(self):
         buf = getenv("I12E_SECRETS_YAML")
         if buf is None or len(buf) < 1:
             raise Exception("undefined 'I12E_SECRETS_YAML' env-var")
-        return yaml.safe_load(decompress(b64decode(buf)))["env"][self.__env]
+        self.__cfg = yaml.safe_load(decompress(b64decode(buf)))
+
+    def secrets_yaml(self):
+        return self.__cfg
 
     def rclone_config(self):
-        c = self.secrets_yaml()
-        return Rclone(c["rclone_remote"],c["rclone_config_pass"]).config()
+        return Rclone(self.__cfg["rclone_remote"],self.__cfg["rclone_config_pass"]).config()
--- genesis/run.sh
+++ genesis/run.sh
@@ -7,7 +7,14 @@ function error_and_exit {
 
 set -e -o pipefail
 
+cd "$(dirname "$0")"
+
 [ "$I12E_ENV" != "prod" ] && I12E_ENV="dev"
+
+SFILE="../secrets-${I12E_ENV}.yaml"
+[ -f "$SFILE" ] || error_and_exit "cannot find '${SFILE}' file"
+I12E_SECRETS_YAML="$(sops decrypt "$SFILE" | gzip | base64 -w 0)"
+[ "$I12E_SECRETS_YAML" = "" ] && error_and_exit "cannot get content from '${SFILE}' file"
 
 [ "$RCLONE_CONFIG_FILE" = "" ] && RCLONE_CONFIG_FILE="${HOME}/.config/rclone/rclone.conf"
 [ -f "$RCLONE_CONFIG_FILE" ] || error_and_exit "RCLONE_CONFIG_FILE='$RCLONE_CONFIG_FILE' file doesn't exist"
@@ -18,13 +25,10 @@ case "$1" in
   *) T_OPT="" ;;
 esac
 
-cd "$(dirname "$0")"
-
 exec docker run -i$T_OPT --rm \
   -v "${RCLONE_CONFIG_FILE}:/root/.config/rclone/rclone.conf:ro" \
   -v ./app:/app/genesis:ro \
-  -e "I12E_ENV=$I12E_ENV" \
-  -e "I12E_SECRETS_YAML=$(sops decrypt ../secrets.yaml | gzip | base64 -w 0)" \
+  -e "I12E_SECRETS_YAML=$I12E_SECRETS_YAML" \
   -e "PYTHONUNBUFFERED=1" \
   -e "PYTHONPATH=/app" \
   ghcr.io/sfmunoz/k8s-bulk:v1.8.0 \
