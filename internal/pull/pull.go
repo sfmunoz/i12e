5e77dd4cfaf182969fa3193f858cc6769bad8506
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-10 16:08:03 +0000

    i12e: '/etc/systemd/system/k3s.service.d/override.conf' using '/etc/i12e/mode' info

--- internal/pull/pull.go
+++ internal/pull/pull.go
@@ -30,8 +30,10 @@ FLAG_FILE="/etc/i12e/config-patched"
 [ -f "$FLAG_FILE" ] && exit 0
 MODE_FILE="/etc/i12e/mode"
 [ -f "$MODE_FILE" ] || exit 0
+MODE="$(cat /etc/i12e/mode)"
 set -x
-cat "/etc/i12e/k3s/config-$(cat $MODE_FILE).yaml" > /etc/rancher/k3s/config.yaml
+cat /etc/i12e/k3s/config-${MODE}.yaml > /etc/rancher/k3s/config.yaml
+cat /etc/i12e/k3s/override-${MODE}.conf > /etc/systemd/system/k3s.service.d/override.conf
 IFACE="{{ .Iface }}"
 IP="{{ .Ip }}"
 if [ "$IFACE" != "" -a "$IP" != "" ]
