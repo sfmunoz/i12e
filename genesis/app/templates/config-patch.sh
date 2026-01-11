d4fa2b08507794893610409cdfc47198e2633f1d
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-11 09:02:57 +0000

    genesis: 'config-patch.sh' fixed to delete 'node-ip/flannel-iface' when needed

--- genesis/app/templates/config-patch.sh
+++ genesis/app/templates/config-patch.sh
@@ -6,23 +6,15 @@ MODE_FILE="/etc/i12e/mode"
 [ -f "$MODE_FILE" ] || exit 0
 MODE="$(cat /etc/i12e/mode)"
 set -x
-cat /etc/i12e/k3s/config-${MODE}.yaml > /etc/rancher/k3s/config.yaml
 cat /etc/i12e/k3s/override-${MODE}.conf > /etc/systemd/system/k3s.service.d/override.conf
-IFACE="$1"
-IP="$2"
-if [ "$IFACE" != "" -a "$IP" != "" ]
-then
-  awk \
-    -v IFACE="$IFACE" \
-    -v IP="$IP" \
-    '!/^(node-ip|flannel-iface):/ {
-      print
-    }
-    END {
+awk \
+  -v IFACE="$1" \
+  -v IP="$2" \
+  '!/^(node-ip|flannel-iface):/ { print }
+  END {
+    if (IFACE != "" && IP != "") {
       printf("flannel-iface: \"%s\"\nnode-ip: \"%s\"\n",IFACE,IP)
-    }' \
-    /etc/rancher/k3s/config.yaml > /etc/rancher/k3s/config.yaml.new
-  cat /etc/rancher/k3s/config.yaml.new > /etc/rancher/k3s/config.yaml
-  rm -f /etc/rancher/k3s/config.yaml.new
-fi
+    }
+  }' \
+  /etc/i12e/k3s/config-${MODE}.yaml > /etc/rancher/k3s/config.yaml
 touch "$FLAG_FILE"
