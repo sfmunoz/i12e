293a7f8816c7f36e22292dcf324dc922294798f2
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-11 08:47:38 +0000

    genesis: 'i12e-k3s-install.sh' renamed to 'k3s-install.sh'

--- genesis/app/artifact.py
+++ genesis/app/artifact.py
@@ -25,7 +25,7 @@ class Artifact(object):
         self.__tpl_k3s_config_yaml = self.__env.get_template("k3s-config.yaml")
         self.__tpl_k3s_override_conf = self.__env.get_template("k3s-override.conf")
         self.__tpl_systemd_genesis_conf = self.__env.get_template("systemd-genesis.conf")
-        self.__tpl_i12e_k3s_install = self.__env.get_template("i12e-k3s-install.sh")
+        self.__tpl_k3s_install = self.__env.get_template("k3s-install.sh")
         self.__tpl_config_patch = self.__env.get_template("config-patch.sh")
         self.__time = time()
 
@@ -147,8 +147,8 @@ class Artifact(object):
         tar.addfile(finfo,BytesIO(data))
         log.info("'{0}' added".format(fname))
 
-    def __i12e_k3s_install_sh(self,tar):
-        data = (self.__tpl_i12e_k3s_install.render() + "\n").encode()
+    def __k3s_install_sh(self,tar):
+        data = (self.__tpl_k3s_install.render() + "\n").encode()
         fname = "opt/libexec/i12e/k3s-install.sh"
         finfo = self.__tarinfo(fname)
         finfo.mode = 0o755
@@ -222,7 +222,7 @@ class Artifact(object):
             self.__etc_crictl_yaml(tar)
             self.__opt_bin_e(tar)
             self.__etc_i12e_iface_txt(tar)
-            self.__i12e_k3s_install_sh(tar)
+            self.__k3s_install_sh(tar)
             self.__config_patch_sh(tar)
             self.__etc_i12e_pull_done(tar)
         self.__rclone_push(buf.getvalue())
