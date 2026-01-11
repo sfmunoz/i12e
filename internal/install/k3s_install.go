fe7a25a1f56faa7eeff127ef1a15376a1fd30e23
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-11 09:22:43 +0000

    i12e: 'k3s_install.go' directly runs '/opt/libexec/i12e/k3s-install.sh'

--- internal/install/k3s_install.go
+++ internal/install/k3s_install.go
@@ -4,11 +4,6 @@ import (
 	"github.com/sfmunoz/i12e/internal/cmdutil"
 )
 
-const script = `#!/bin/sh
-[ -x /opt/libexec/i12e/k3s-install.sh ] && exec /opt/libexec/i12e/k3s-install.sh
-exit 0
-`
-
 func K3sInstall() {
-	cmdutil.RunCmd("/bin/sh", "-c", script)
+	cmdutil.RunCmd("/opt/libexec/i12e/k3s-install.sh")
 }
