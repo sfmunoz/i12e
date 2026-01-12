8a8bde89b1ba6df6f48c513aa500bb7eef24d8b3
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-12 18:22:25 +0000

    genesis: 'run.sh' sets 'I12E_BUTANE' env-var with gzipped+base64 'butane-(dev|prod).yaml' content

--- genesis/run.sh
+++ genesis/run.sh
@@ -16,6 +16,13 @@ SECRETS_YAML="../secrets-${I12E_ENV}.yaml"
 I12E_CONFIG="$(sops decrypt "$SECRETS_YAML" | gzip | base64 -w 0)"
 [ "$I12E_CONFIG" = "" ] && error_and_exit "cannot get content from '${SECRETS_YAML}' file"
 
+BUTANE_YAML="../butane-${I12E_ENV}.yaml"
+if [ -f "$BUTANE_YAML" ]
+then
+  I12E_BUTANE="$(sops decrypt "$BUTANE_YAML" | gzip | base64 -w 0)"
+  [ "$I12E_BUTANE" = "" ] && error_and_exit "cannot get content from '${BUTANE_YAML}' file"
+fi
+
 [ "$RCLONE_CONFIG_FILE" = "" ] && RCLONE_CONFIG_FILE="${HOME}/.config/rclone/rclone.conf"
 [ -f "$RCLONE_CONFIG_FILE" ] || error_and_exit "RCLONE_CONFIG_FILE='$RCLONE_CONFIG_FILE' file doesn't exist"
 
@@ -29,6 +36,7 @@ exec docker run -i$T_OPT --rm \
   -v "${RCLONE_CONFIG_FILE}:/root/.config/rclone/rclone.conf:ro" \
   -v ./app:/app/genesis:ro \
   -e "I12E_CONFIG=$I12E_CONFIG" \
+  -e "I12E_BUTANE=$I12E_BUTANE" \
   -e "PYTHONUNBUFFERED=1" \
   -e "PYTHONPATH=/app" \
   ghcr.io/sfmunoz/k8s-bulk:v1.8.0 \
