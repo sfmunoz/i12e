b13a4071c42c37ebb6e47edca8e598246b73ca6e
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-17 10:22:35 +0000

    i12e: boilerplate 'cobra-cli' generated code dropped and command basic help added

--- cmd/artifact.go
+++ cmd/artifact.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,16 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// artifactCmd represents the artifact command
 var artifactCmd = &cobra.Command{
 	Use:   "artifact",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Artifact generation and push with rclone",
+	Long: `Artifact management:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  - generation: tar+gz artifact
+  - push to remote using rclone`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("artifact called")
 	},
@@ -27,14 +20,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(artifactCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
 	// artifactCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	// artifactCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/butane.go
+++ cmd/butane.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -13,13 +9,11 @@ import (
 // butaneCmd represents the butane command
 var butaneCmd = &cobra.Command{
 	Use:   "butane",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Run butane to generate ignition code",
+	Long: `Run butane to generate ignition code:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  - input: butane
+  - output: ignition`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("butane called")
 	},
@@ -27,14 +21,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(butaneCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
 	// butaneCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	// butaneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
