d12510553582fcae3f81d2b667476f99b62b32ac
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-18 11:46:31 +0000

    i12e: config 'type Mode int' turned into 'type Mode string'

--- internal/config/mode.go
+++ internal/config/mode.go
@@ -4,32 +4,32 @@ import (
 	"fmt"
 )
 
-type Mode int
+type Mode string
 
 const (
-	ModeMain Mode = iota
-	ModeServer
-	ModeAgent
+	ModeMain   Mode = "main"
+	ModeServer Mode = "server"
+	ModeAgent  Mode = "agent"
 )
 
-var modeMap = map[Mode]string{
-	ModeMain:   "main",
-	ModeServer: "server",
-	ModeAgent:  "agent",
-}
+var modes = []Mode{ModeMain, ModeServer, ModeAgent}
 
 func (m Mode) String() string {
-	return modeMap[m]
+	return string(m)
 }
 
 func ValidModes() []string {
-	return []string{ModeMain.String(), ModeServer.String(), ModeAgent.String()}
+	ret := make([]string, len(modes))
+	for i, v := range modes {
+		ret[i] = string(v)
+	}
+	return ret
 }
 
 func GetMode(m string) (Mode, error) {
-	for k, v := range modeMap {
-		if v == m {
-			return k, nil
+	for _, v := range modes {
+		if string(v) == m {
+			return v, nil
 		}
 	}
 	return ModeMain, fmt.Errorf("unknown mode '%s' (valid: %q)", m, ValidModes())
