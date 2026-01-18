0c754d7b33bb2b3a09a5fd3d52eff64644d9c2a4
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-18 16:57:15 +0000

    i12e: 'bash_raw.sh' and 'bash_b64.sh' created (not used yet)

---
+++ internal/butane/templates/bash_raw.sh
@@ -0,0 +1,17 @@
+set -x -e -o pipefail
+sudo rm -fv "{{ .ConfigIgn }}"
+base64 -d <<< "{{ .Buf }}" | \
+  gunzip | \
+  sudo flatcar-reset \
+    --keep-machine-id \
+    --keep-paths \
+    '/etc/ssh/ssh_host_.*' \
+    /var/log \
+    /var/lib/rancher/k3s/agent/containerd \
+    /home/core/.bash_history \
+    /root/.bash_history \
+    -F \
+    /dev/stdin
+sudo test -s "{{ .ConfigIgn }}"
+sudo jq . "{{ .ConfigIgn }}"
+sudo systemd-run bash -c 'sleep 1 ; systemctl reboot'
