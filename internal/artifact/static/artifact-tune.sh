7759d4d24114e4a2cced9d035a34d3e86e179c96
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-19 19:41:26 +0000

    'cp genesis/app/templates/artifact-tune.sh internal/artifact/static/artifact-tune.sh' executed

---
+++ internal/artifact/static/artifact-tune.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e -o pipefail
+function artifact_tune {
+  FLAG_FILE="/etc/i12e/flags/artifact-tuned"
+  [ -f "$FLAG_FILE" ] && return 0
+  MODE_FILE="/etc/i12e/mode"
+  [ -f "$MODE_FILE" ] || return 0
+  MODE="$(cat /etc/i12e/mode)"
+  set -x
+  cat /etc/i12e/k3s/override-${MODE}.conf > /etc/systemd/system/k3s.service.d/override.conf
+  awk \
+    -v IFACE="$1" \
+    -v IP="$2" \
+    '!/^(node-ip|flannel-iface):/ { print }
+    END {
+      if (IFACE != "" && IP != "") {
+        printf("flannel-iface: \"%s\"\nnode-ip: \"%s\"\n",IFACE,IP)
+      }
+    }' \
+    /etc/i12e/k3s/config-${MODE}.yaml > /etc/rancher/k3s/config.yaml
+  systemctl daemon-reload
+  systemctl restart update-engine
+  systemctl restart nftables
+  systemctl enable nftables
+  #systemctl restart locksmithd
+  touch "$FLAG_FILE"
+  { set +x; } 2> /dev/null
+}
+function k3s_install {
+  [ -f "/etc/systemd/system/k3s.service" ] && return 0
+  echo "Installing k3s..."
+  set -x
+  export INSTALL_K3S_VERSION="v1.34.3+k3s1"
+  curl -sfL https://get.k3s.io | sh -s
+  { set +x; } 2> /dev/null
+}
+artifact_tune "$1" "$2"
+k3s_install
