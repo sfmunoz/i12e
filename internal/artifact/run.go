6de728789aad8cb9a410a149132ed2bc803ca237
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-20 18:03:33 +0000

    i12e: 'Artifact' struct created and used

--- internal/artifact/run.go
+++ internal/artifact/run.go
@@ -18,7 +18,40 @@ var log = logit.Logit().WithLevel(logit.LevelInfo)
 //go:embed static/* templates/*
 var FS embed.FS
 
-func folders(tarOut *tar.Writer, tnow *time.Time) error {
+type Artifact struct {
+	tnow   time.Time
+	tarOut *tar.Writer
+	gzOut  *gzip.Writer
+	obuf   *bytes.Buffer
+}
+
+func newArtifact() *Artifact {
+	var obuf bytes.Buffer
+	gzOut := gzip.NewWriter(&obuf)
+	tarOut := tar.NewWriter(gzOut)
+	return &Artifact{
+		tnow:   time.Now().UTC(),
+		tarOut: tarOut,
+		gzOut:  gzOut,
+		obuf:   &obuf,
+	}
+}
+
+func (a *Artifact) Close() error {
+	if err := a.tarOut.Close(); err != nil {
+		return err
+	}
+	if err := a.gzOut.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *Artifact) Obuf() *bytes.Buffer {
+	return a.obuf
+}
+
+func (a *Artifact) folders() error {
 	flist := []struct {
 		Name string
 		Mode int64
@@ -35,60 +68,29 @@ func folders(tarOut *tar.Writer, tnow *time.Time) error {
 		hdr := &tar.Header{
 			Typeflag: tar.TypeDir,
 			Name:     folder.Name,
-			ModTime:  *tnow,
+			ModTime:  a.tnow,
 			Mode:     folder.Mode,
 			Uid:      0,
 			Gid:      0,
 			Uname:    "root",
 			Gname:    "root",
 		}
-		if err := tarOut.WriteHeader(hdr); err != nil {
+		if err := a.tarOut.WriteHeader(hdr); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func tarGz() (*bytes.Buffer, error) {
-	var buf bytes.Buffer
-	gzOut := gzip.NewWriter(&buf)
-	tarOut := tar.NewWriter(gzOut)
-	tnow := time.Now().UTC()
-	folders(tarOut, &tnow)
-	var files = []struct {
-		Name, Body string
-	}{
-		{"file1.txt", "1st file"},
-		{"file2.txt", "2nd file"},
-		{"file3.txt", "3rd and last file"},
-	}
-	for _, file := range files {
-		hdr := &tar.Header{
-			Typeflag: tar.TypeReg,
-			Name:     file.Name,
-			ModTime:  tnow,
-			Size:     int64(len(file.Body)),
-			Mode:     0644,
-			Uid:      0,
-			Gid:      0,
-			Uname:    "root",
-			Gname:    "root",
-		}
-		if err := tarOut.WriteHeader(hdr); err != nil {
-			return nil, err
-		}
-		if _, err := tarOut.Write([]byte(file.Body)); err != nil {
-			return nil, err
-		}
-	}
+func (a *Artifact) optBinE() error {
 	body, err := FS.ReadFile("static/opt-bin-e")
 	if err != nil {
-		return nil, err
+		return err
 	}
 	hdr := &tar.Header{
 		Typeflag: tar.TypeReg,
 		Name:     "opt/bin/e",
-		ModTime:  tnow,
+		ModTime:  a.tnow,
 		Size:     int64(len(body)),
 		Mode:     0755,
 		Uid:      0,
@@ -96,29 +98,30 @@ func tarGz() (*bytes.Buffer, error) {
 		Uname:    "root",
 		Gname:    "root",
 	}
-	if err := tarOut.WriteHeader(hdr); err != nil {
-		return nil, err
+	if err := a.tarOut.WriteHeader(hdr); err != nil {
+		return err
 	}
-	if _, err := tarOut.Write(body); err != nil {
-		return nil, err
+	if _, err := a.tarOut.Write(body); err != nil {
+		return err
 	}
-	if err := tarOut.Close(); err != nil {
-		return nil, err
-	}
-	if err := gzOut.Close(); err != nil {
-		return nil, err
-	}
-	return &buf, nil
+	return nil
 }
 
 func Run(cfg *config.Config) error {
 	if cfg == nil {
 		return fmt.Errorf("artifact.Run(): undefined config")
 	}
-	buf, err := tarGz()
-	if err != nil {
+	a := newArtifact()
+	funcList := []func() error{a.folders, a.optBinE}
+	for _, f := range funcList {
+		if err := f(); err != nil {
+			a.Close()
+			return err
+		}
+	}
+	if err := a.Close(); err != nil {
 		return err
 	}
-	fmt.Fprint(os.Stdout, buf.String())
+	fmt.Fprint(os.Stdout, a.Obuf().String())
 	return nil
 }
