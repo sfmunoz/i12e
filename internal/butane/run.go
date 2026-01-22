bac352f30c2765d0cf1e5cb96d674e98b32f3ffc
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-22 18:20:37 +0000

    i12e: 'Butane' struct created and related methods attached to it

--- internal/butane/run.go
+++ internal/butane/run.go
@@ -19,9 +19,20 @@ var log = logit.Logit().WithLevel(logit.LevelInfo)
 var i12eVersion = "v0.0.18"
 var i12eSha256Sum = "cfe8d33bc00805344dbe4008d87b896ea0c3bb0618cc69bcf5bc0462af4a2709"
 
-func ignitionConfigMergeSource(cfg *config.Config) (*bytes.Buffer, error) {
+type Butane struct {
+	cfg *config.Config
+}
+
+func newButane(cfg *config.Config) (*Butane, error) {
+	if cfg == nil {
+		return nil, fmt.Errorf("newButane(): undefined config")
+	}
+	return &Butane{cfg: cfg}, nil
+}
+
+func (b *Butane) ignitionConfigMergeSource() (*bytes.Buffer, error) {
 	fname := "butane-dev.yaml"
-	if cfg.Prod {
+	if b.cfg.Prod {
 		fname = "butane-prod.yaml"
 	}
 	_, err := os.Stat(fname)
@@ -33,13 +44,13 @@ func ignitionConfigMergeSource(cfg *config.Config) (*bytes.Buffer, error) {
 	}
 	bo1, be1, err := cmdutil.RunSimple(exec.Command("sops", "decrypt", fname))
 	if err != nil {
-		return nil, fmt.Errorf("'sops decrypt' failed: err=%s; buf_err=%s; prod=%t", err, be1, cfg.Prod)
+		return nil, fmt.Errorf("'sops decrypt' failed: err=%s; buf_err=%s; prod=%t", err, be1, b.cfg.Prod)
 	}
-	cmd := butaneCmd(cfg)
+	cmd := butaneCmd(b.cfg)
 	cmd.Stdin = bo1
 	bo2, be2, err := cmdutil.RunSimple(cmd)
 	if err != nil {
-		return nil, fmt.Errorf("'butane' failed: err=%s; buf_err=%s; prod=%t", err, be2, cfg.Prod)
+		return nil, fmt.Errorf("'butane' failed: err=%s; buf_err=%s; prod=%t", err, be2, b.cfg.Prod)
 	}
 	var ret bytes.Buffer
 	_, err = ret.WriteString("data:;base64,")
@@ -58,16 +69,16 @@ func ignitionConfigMergeSource(cfg *config.Config) (*bytes.Buffer, error) {
 	return &ret, nil
 }
 
-func butaneRender(cfg *config.Config) (*bytes.Buffer, error) {
+func (b *Butane) butaneRender() (*bytes.Buffer, error) {
 	tpl, err := tplNew("flatcar.yaml", true)
 	if err != nil {
 		return nil, err
 	}
-	icms, err := ignitionConfigMergeSource(cfg)
+	icms, err := b.ignitionConfigMergeSource()
 	if err != nil {
 		return nil, err
 	}
-	rcloneConfig, err := RcloneConfig(cfg)
+	rcloneConfig, err := RcloneConfig(b.cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -82,8 +93,8 @@ func butaneRender(cfg *config.Config) (*bytes.Buffer, error) {
 		IgnitionConfigMergeSource: icms,
 		I12eVersion:               i12eVersion,
 		I12eSha256sum:             i12eSha256Sum,
-		Mode:                      cfg.Mode.String(),
-		SshAuthorizedKeys:         cfg.SshAuthorizedKeys,
+		Mode:                      b.cfg.Mode.String(),
+		SshAuthorizedKeys:         b.cfg.SshAuthorizedKeys,
 		RcloneConf:                rcloneConfig,
 	}
 	var ret bytes.Buffer
@@ -91,7 +102,7 @@ func butaneRender(cfg *config.Config) (*bytes.Buffer, error) {
 	if err != nil {
 		return nil, err
 	}
-	if cfg.Bout == config.BoutDebug {
+	if b.cfg.Bout == config.BoutDebug {
 		log.Info("======== butane begin ========")
 		for _, line := range strings.Split(ret.String(), "\n") {
 			log.Info(line)
@@ -101,14 +112,14 @@ func butaneRender(cfg *config.Config) (*bytes.Buffer, error) {
 	return &ret, nil
 }
 
-func ignitionRender(cfg *config.Config, buf *bytes.Buffer) (*bytes.Buffer, error) {
-	cmd := butaneCmd(cfg)
+func (b *Butane) ignitionRender(buf *bytes.Buffer) (*bytes.Buffer, error) {
+	cmd := butaneCmd(b.cfg)
 	cmd.Stdin = buf
 	bo, be, err := cmdutil.RunSimple(cmd)
 	if err != nil {
-		return nil, fmt.Errorf("'butane' failed: err=%s; buf_err=%s; prod=%t", err, be, cfg.Prod)
+		return nil, fmt.Errorf("'butane' failed: err=%s; buf_err=%s; prod=%t", err, be, b.cfg.Prod)
 	}
-	if cfg.Bout == config.BoutDebug {
+	if b.cfg.Bout == config.BoutDebug {
 		log.Info("======== ignition begin ========")
 		for _, line := range strings.Split(bo.String(), "\n") {
 			log.Info(line)
@@ -118,7 +129,7 @@ func ignitionRender(cfg *config.Config, buf *bytes.Buffer) (*bytes.Buffer, error
 	return bo, nil
 }
 
-func bashRaw(cfg *config.Config, ibuf *bytes.Buffer) (*bytes.Buffer, error) {
+func (b *Butane) bashRaw(ibuf *bytes.Buffer) (*bytes.Buffer, error) {
 	gzBuf, err := b64Gzip(ibuf)
 	if err != nil {
 		return nil, err
@@ -142,7 +153,7 @@ func bashRaw(cfg *config.Config, ibuf *bytes.Buffer) (*bytes.Buffer, error) {
 	return &ret, nil
 }
 
-func bashB64(cfg *config.Config, ibuf *bytes.Buffer) (*bytes.Buffer, error) {
+func (b *Butane) bashB64(ibuf *bytes.Buffer) (*bytes.Buffer, error) {
 	gzBuf, err := b64Gzip(ibuf)
 	if err != nil {
 		return nil, err
@@ -160,40 +171,45 @@ func bashB64(cfg *config.Config, ibuf *bytes.Buffer) (*bytes.Buffer, error) {
 	return &ret, nil
 }
 
-func Run(cfg *config.Config) error {
-	if cfg == nil {
-		return fmt.Errorf("butane.Run(): undefined config")
-	}
-	buf, err := butaneRender(cfg)
+func (b *Butane) run() error {
+	buf, err := b.butaneRender()
 	if err != nil {
 		return err
 	}
-	ignitionBuf, err := ignitionRender(cfg, buf)
+	ignitionBuf, err := b.ignitionRender(buf)
 	if err != nil {
 		return err
 	}
-	if cfg.Bout == config.BoutDebug {
+	if b.cfg.Bout == config.BoutDebug {
 		return nil
 	}
-	if cfg.Bout == config.BoutIgnition {
+	if b.cfg.Bout == config.BoutIgnition {
 		fmt.Fprint(os.Stdout, ignitionBuf.String())
 		return nil
 	}
-	bufRaw, err := bashRaw(cfg, ignitionBuf)
+	bufRaw, err := b.bashRaw(ignitionBuf)
 	if err != nil {
 		return err
 	}
-	if cfg.Bout == config.BoutBashRaw {
+	if b.cfg.Bout == config.BoutBashRaw {
 		fmt.Fprint(os.Stdout, bufRaw.String())
 		return nil
 	}
-	bufB64, err := bashB64(cfg, bufRaw)
+	bufB64, err := b.bashB64(bufRaw)
 	if err != nil {
 		return err
 	}
-	if cfg.Bout == config.BoutBashB64 {
+	if b.cfg.Bout == config.BoutBashB64 {
 		fmt.Fprint(os.Stdout, bufB64.String())
 		return nil
 	}
 	return nil
+}
+
+func Run(cfg *config.Config) error {
+	b, err := newButane(cfg)
+	if err != nil {
+		return err
+	}
+	return b.run()
 }
