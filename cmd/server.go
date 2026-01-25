2d67cdbf4b1862a7bc062de71c15cff6b8b191ed
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-25 09:19:34 +0000

    i12e: 'internal/server' package created (bound to 'server' command)

--- cmd/server.go
+++ cmd/server.go
@@ -1,40 +1,34 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/sfmunoz/i12e/internal/config"
+	"github.com/sfmunoz/i12e/internal/server"
 	"github.com/spf13/cobra"
 )
+
+func serverRun(cmd *cobra.Command, args []string) error {
+	prod, err := cmd.Flags().GetBool("prod")
+	if err != nil {
+		return err
+	}
+	cfg, err := config.LoadConfig(prod)
+	if err != nil {
+		return err
+	}
+	return server.Run(cfg)
+}
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Server to run on target hosts",
+	Long: `Run server on target hosts:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("server called")
-	},
+  - pulls artifacts from rclone server
+  - configures network`,
+	RunE: serverRun,
 }
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
