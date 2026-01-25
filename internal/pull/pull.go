f987bc879d0a8af3f54de0efe5a261c5e9ad2e32
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-25 18:27:36 +0000

    i12e: 'pull.go' uses 'Pull' struct to manage internal info

--- internal/pull/pull.go
+++ internal/pull/pull.go
@@ -15,12 +15,18 @@ rclone cat rem:artifact.tar.gz | tar -C / -xvz
 rm -f /etc/i12e/flags/artifact-tuned
 `
 
-func Pull() {
+type Pull struct{}
+
+func newPull() *Pull {
+	return &Pull{}
+}
+
+func (p *Pull) run() error {
 	log.Info("Pull()...")
 	err := cmdutil.RunCmd("/bin/sh", "-c", rcloneScript)
 	if err != nil {
 		log.Error("rcloneScript failed", "err", err)
-		return
+		return err
 	}
 	iface := ""
 	ip := ""
@@ -32,5 +38,11 @@ func Pull() {
 	err = cmdutil.RunCmd("/opt/libexec/i12e/artifact-tune.sh", iface, ip)
 	if err != nil {
 		log.Error("/opt/libexec/i12e/artifact-tune.sh failed", "err", err, "iface", iface, "ip", ip)
+		return err
 	}
+	return nil
+}
+
+func Run() error {
+	return newPull().run()
 }
