ebcf498191b7a8f88fe69a5517f97824794cec42
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-01-28 15:30:54 +0000

    i12e: 'Net.push()' moved to 'Mesh.NodePush()'

--- internal/net/mesh.go
+++ internal/net/mesh.go
@@ -1,9 +1,11 @@
 package net
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/sfmunoz/i12e/internal/cmdutil"
 )
@@ -15,6 +17,26 @@ type Mesh struct {
 
 func (m *Mesh) String() string {
 	return strings.Join(m.data, "\n")
+}
+
+func (m *Mesh) NodePush(nodePath string, ts time.Time, wgPubKey *WgKey, wgEndpointIp string, wgEndpointPort uint16) error {
+	cmd := exec.Command(
+		"/bin/sh",
+		"-s",
+		"-",
+		"node-push",
+		nodePath,
+		fmt.Sprintf("%s_%09d", ts.Format("20060102_150405"), ts.Nanosecond()),
+		wgPubKey.Hex(),
+		wgEndpointIp,
+		fmt.Sprintf("%d", wgEndpointPort),
+	)
+	cmd.Stdin = bytes.NewBuffer(netSh)
+	bo, be, err := cmdutil.RunSimple(cmd)
+	if err != nil {
+		return fmt.Errorf("Net.push(): 'net.sh' failed': %s (stdout=%s, stderr=%s)", err, bo.String(), be.String())
+	}
+	return nil
 }
 
 func getMesh(base string) (*Mesh, error) {
--- internal/net/net.go
+++ internal/net/net.go
@@ -1,13 +1,9 @@
 package net
 
 import (
-	"bytes"
 	_ "embed"
-	"fmt"
-	"os/exec"
 	"time"
 
-	"github.com/sfmunoz/i12e/internal/cmdutil"
 	"github.com/sfmunoz/i12e/internal/netutil"
 	"github.com/sfmunoz/logit"
 )
@@ -63,26 +59,6 @@ func newNet() (*Net, error) {
 	}, nil
 }
 
-func (n *Net) push() error {
-	cmd := exec.Command(
-		"/bin/sh",
-		"-s",
-		"-",
-		"node-push",
-		n.MachineId.PathName(),
-		fmt.Sprintf("%s_%09d", n.Tnow.Format("20060102_150405"), n.Tnow.Nanosecond()),
-		n.WgPubKey.Hex(),
-		n.WgEndpointIp,
-		fmt.Sprintf("%d", n.WgEndpointPort),
-	)
-	cmd.Stdin = bytes.NewBuffer(netSh)
-	bo, be, err := cmdutil.RunSimple(cmd)
-	if err != nil {
-		return fmt.Errorf("Net.push(): 'net.sh' failed': %s (stdout=%s, stderr=%s)", err, bo.String(), be.String())
-	}
-	return nil
-}
-
 func (n *Net) run() error {
 	log.Info("Net.run()", "Tnow", n.Tnow)
 	log.Info("Net.run()", "MachineId", n.MachineId, "NodeId", n.MachineId.NodeId(), "NodeName", n.MachineId.NodeName(), "PathName", n.MachineId.PathName(), "IP", n.MachineId.IP())
@@ -91,11 +67,11 @@ func (n *Net) run() error {
 	log.Info("Net.run()", "WgEndpointPort", n.WgEndpointPort)
 	log.Info("Net.run()", "WgPrivKey", n.WgPrivKey, "WgPrivKeyLen", n.WgPrivKey.Len())
 	log.Info("Net.run()", "WgPubKey", n.WgPubKey, "WgPubKeyHex", n.WgPubKey.Hex(), "WgPubKeyLen", n.WgPubKey.Len())
-	if err := n.push(); err != nil {
-		return err
-	}
 	mesh, err := getMesh("rem:mesh")
 	if err != nil {
+		return err
+	}
+	if err := mesh.NodePush(n.MachineId.PathName(), n.Tnow, n.WgPubKey, n.WgEndpointIp, n.WgEndpointPort); err != nil {
 		return err
 	}
 	log.Info("Net.run()", "mesh", mesh)
