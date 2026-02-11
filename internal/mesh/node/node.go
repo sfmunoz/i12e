a671135bd70338112dc2b70674d2234f216009e3
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-02-11 18:15:08 +0000

    i12e: 'node_id_name.go' merged into 'node.go' and deleted

--- internal/mesh/node/node.go
+++ internal/mesh/node/node.go
@@ -1,9 +1,43 @@
 package node
 
+import (
+	"fmt"
+	"net/netip"
+	"strconv"
+)
+
 const nodeEndpointPort = 51830 // default '51820'
 const nodeInterface = "wgi"
 const nodePrivKeyFname = "/etc/i12e/wg-priv-key"
 
 func GetNodeInterface() string {
 	return nodeInterface
+}
+
+func getNodeNameFromNodeId(nodeId uint32) string {
+	s := strconv.FormatUint(uint64(nodeId), 36)
+	for len(s) < 4 {
+		s = "0" + s
+	}
+	return "n" + s
+}
+
+func getNodeIdFromNodeName(meshNet *netip.Prefix, nodeName string) (uint32, error) {
+	nodeNameLen := len(nodeName)
+	if nodeNameLen != 5 {
+		return 0, fmt.Errorf("len(nodename=%s)=%d (5 expected)", nodeName, nodeNameLen)
+	}
+	n0 := nodeName[0]
+	if n0 != 'n' {
+		return 0, fmt.Errorf("nodename='%s' starts with '%c' ('n' expected)", nodeName, n0)
+	}
+	nodeIdInt64, err := strconv.ParseInt(nodeName[1:], 36, 32)
+	if err != nil {
+		return 0, err
+	}
+	nodeId := uint32(nodeIdInt64)
+	if err := nodeIdValid(meshNet, nodeId); err != nil {
+		return 0, err
+	}
+	return nodeId, nil
 }
