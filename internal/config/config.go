2e084770419a8572454bb93311327d3bdad145f6
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-02-22 10:31:47 +0000

    i12e: validators moved from 'config.go' to 'validators.go'

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,6 @@ package config
 import (
 	"fmt"
 	"net/netip"
-	"slices"
-	"strings"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -87,44 +85,4 @@ func (c *Config) I12eYaml() string {
 
 func (c *Config) I12eEncYaml() string {
 	return c.fname("i12e.enc.yaml")
-}
-
-func semverV(fl validator.FieldLevel) bool {
-	s1 := fl.Field().String()
-	s2 := strings.TrimPrefix(s1, "v")
-	return validator.New().Var(s2, "semver") == nil
-}
-
-func validMode(fl validator.FieldLevel) bool {
-	return slices.Contains(ValidModes(), fl.Field().String())
-}
-
-func validOutput(fl validator.FieldLevel) bool {
-	return slices.Contains(ValidOutputs(), fl.Field().String())
-}
-
-func validMeshNetwork(fl validator.FieldLevel) bool {
-	p, ok := fl.Field().Addr().Interface().(*netip.Prefix)
-	if !ok {
-		return false
-	}
-	meshAddr := p.Addr()
-	if !meshAddr.Is4() {
-		fmt.Printf("config: 'mesh.network_address=%s' is not IPv4\n", p)
-		return false
-	}
-	if !meshAddr.IsPrivate() {
-		fmt.Printf("config: 'mesh.network_address=%s' is not private\n", p)
-		return false
-	}
-	b := p.Bits() // from /12 (20 bits for host) to /29 (3 bits for host)
-	if b < 12 {
-		fmt.Printf("config: wrong 'mesh.network_address=%s' (bits=%d, min=12)\n", p, b)
-		return false
-	}
-	if b > 29 {
-		fmt.Printf("config: wrong 'mesh.network_address=%s' (bits=%d, max=29)\n", p, b)
-		return false
-	}
-	return true
 }
