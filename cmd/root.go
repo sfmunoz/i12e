068615f8356f1550a7800e65c217c62ac0ce6945
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-02-23 18:25:53 +0000

    i12e: 'PrefixDecodeHook()' moved from 'cmd/hooks.go' to 'cmd/root.go'; 'cmd/hooks.go' deleted

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,33 @@
 package cmd
 
 import (
+	"fmt"
+	"net/netip"
 	"os"
+	"reflect"
 	"time"
 
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
+
+func PrefixDecodeHook() mapstructure.DecodeHookFunc {
+	return func(from reflect.Type, to reflect.Type, data any) (any, error) {
+		if from.Kind() != reflect.String {
+			return data, nil
+		}
+		if to != reflect.TypeFor[*netip.Prefix]() {
+			return data, nil
+		}
+		s := data.(string)
+		p, err := netip.ParsePrefix(s)
+		if err != nil {
+			return nil, fmt.Errorf("invalid prefix %q: %w", s, err)
+		}
+		return &p, nil
+	}
+}
 
 func viperNew() *viper.Viper {
 	v := viper.New()
