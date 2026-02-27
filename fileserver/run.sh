48aeace277626f53d91d75960f041bfe4fa1b127
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-02-27 19:49:15 +0000

    fileserver/run.sh: 'rustfs' binary is used instead of 'rustfs' docker image

--- fileserver/run.sh
+++ fileserver/run.sh
@@ -8,7 +8,7 @@
 #     $ ./rustfs --version
 #
 
-set -e
+set -e -o pipefail
 cd "$(dirname "$0")"
 
 if [ ! -f rustfs ]; then
@@ -20,27 +20,18 @@ if [ ! -f rustfs ]; then
   { set +x; } 2>/dev/null
 fi
 
-sudo -u root mkdir -p data logs
-sudo -u root chown -R 10001:10001 data logs
+set -x
+mkdir -p data
 { set +x; } 2>/dev/null
-set +e
 
-DATA_FOLDER="$(pwd)/data"
-LOGS_FOLDER="$(pwd)/logs"
+set +e
 
 while true; do
   set -x
-  docker run \
-    -it \
-    --rm \
-    --name rustfs \
-    -p 127.0.0.1:9000:9000 \
-    -p 127.0.0.1:9001:9001 \
-    -p 192.168.56.1:9000:9000 \
-    -p 192.168.56.1:9001:9001 \
-    -v ${DATA_FOLDER}:/data \
-    -v ${LOGS_FOLDER}:/logs \
-    rustfs/rustfs:latest
+  ./rustfs \
+    --address 192.168.56.1:9000 \
+    --console-address 192.168.56.1:9001 \
+    data
   { set +x; } 2>/dev/null
   echo "waiting 1s to try again..."
   sleep 1
