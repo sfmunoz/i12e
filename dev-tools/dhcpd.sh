511f3356a783f46bdedcdc193c5cf46fa241e863
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-02-28 08:41:24 +0000

    dev-tools: 'udhcpd.conf' renamed to 'dhcpd.conf'

--- dev-tools/dhcpd.sh
+++ dev-tools/dhcpd.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-while true
-do
+while true; do
   set -x
-  awk '!/^(#|$)/' udhcpd.conf
-  sudo busybox udhcpd -f udhcpd.conf
+  awk '!/^(#|$)/' dhcpd.conf
+  sudo busybox udhcpd -f dhcpd.conf
   { set +x; } 2>/dev/null
   echo "waiting 1s to try again..."
   sleep 1
