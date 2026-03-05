3383da9229ed7eed6847d875045b24f42033fed0
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-05 17:46:09 +0000

    i12e: 'Server.runOne()' created to simplify 'Server.run()' code

--- internal/server/server.go
+++ internal/server/server.go
@@ -20,25 +20,27 @@ func newServer(cfg *config.ServerConfig) *Server {
 	return &Server{cfg}
 }
 
+func (s *Server) runOne() {
+	if err := rclonePull(); err != nil {
+		log.Error("rclonePull() failed", "err", err)
+		return
+	}
+	if err := mesh.Run(s.cfg); err != nil {
+		log.Error("mesh.Run() failed", "err", err)
+	}
+	if err := pull.Run(s.cfg); err != nil {
+		log.Error("pull.Run() failed", "err", err)
+	}
+}
+
 func (s *Server) run() error {
 	i := 0
 	for {
+		s.runOne()
 		slumber := s.cfg.Server.SlumberBase + time.Duration(rand.Int64N(int64(s.cfg.Server.SlumberJitter)))
 		if i < 4 {
 			i += 1
 			slumber = 16*time.Second + time.Duration(rand.Int64N(int64(4*time.Second)))
-		}
-		if err := rclonePull(); err != nil {
-			log.Error("rclonePull() failed", "err", err)
-			log.Info("i12e sleeping...", "slumber", slumber)
-			time.Sleep(slumber)
-			continue
-		}
-		if err := mesh.Run(s.cfg); err != nil {
-			log.Error("mesh.Run() failed", "err", err)
-		}
-		if err := pull.Run(s.cfg); err != nil {
-			log.Error("pull.Run() failed", "err", err)
 		}
 		log.Info("i12e sleeping...", "slumber", slumber)
 		time.Sleep(slumber)
