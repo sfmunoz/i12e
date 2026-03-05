7a0aad662d09b111917f45aea9962ee372db31d5
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-05 17:51:48 +0000

    i12e: 'rclone-pull.sh' doesn't execute 'sha256sum' when there's no '/opt/bin/rclone' executable

--- internal/server/static/rclone-pull.sh
+++ internal/server/static/rclone-pull.sh
@@ -3,7 +3,12 @@ VERSION="v1.73.1"
 SHA256SUM="10ffca7640f0483beba9a9c9f4cdb6a9ca30a13c4ab31f2329d09a832802694d"
 RCLONE_BIN="/opt/bin/rclone"
 set -x
-[ -x "${RCLONE_BIN}" -a "$(sha256sum "${RCLONE_BIN}")" = "${SHA256SUM}  ${RCLONE_BIN}" ] && exit 0
+# if+if, no '-a' -> sha256sum would be executed otherwise
+if [ -x "${RCLONE_BIN}" ]; then
+  if [ "$(sha256sum "${RCLONE_BIN}")" = "${SHA256SUM}  ${RCLONE_BIN}" ]; then
+    exit 0
+  fi
+fi
 set -e -o pipefail
 rm -rf "${RCLONE_BIN}"
 curl -sfL "https://github.com/rclone/rclone/releases/download/${VERSION}/rclone-${VERSION}-linux-amd64.zip" |
