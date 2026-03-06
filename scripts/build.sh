3147d4a6a4fe6d39160c4b07c804027ec8937bcf
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-06 18:21:32 +0000

    build.sh: copied from 'https://github.com/sfmunoz/github-playground/blob/v0.0.29/scripts/build.sh'

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,72 +1,38 @@
 #!/bin/bash
-#
-# Refs:
-# - https://www.flatcar.org/docs/latest/provisioning/sysext/
-# - https://github.com/sfmunoz/i12e/issues/70
-# - https://github.com/sfmunoz/i12e/issues/83
-# - https://github.com/sfmunoz/i12e/issues/85
-#
-# Proof-of-concept:
-# - i12e-flatcar extension generation
-# - tmux (with required 'libutempter.so*') and rclone are included
-#
-# Tested on:
-# - Source: Linux Mint 22.2 Cinnamon
-# - Target: Flatcar 4459.2.2
-#
-# tmux works despite the following warning:
-#   $ tmux
-#   tmux: /usr/lib64/libtinfo.so.6: no version information available (required by tmux)
-#
-# Req on Linux Mint 22.2:
-#   # apt install squashfs-tools erofs-utils tmux rclone
-#
-# FS=squashfs is the default for now since compression is better:
-#   [FS=squashfs] ./scripts/i12e-ext.sh ---> du -s build/i12e-flatcar.raw = 17228
-#   FS=erofs ./scripts/i12e-ext.sh --------> du -s build/i12e-flatcar.raw = 27448
-#
-
-[ "$FS" = "" ] && FS="squashfs"
-
-case "$FS" in
-squashfs | erofs) ;;
-*)
-  echo "error: unsupported FS='$FS'; it must be 'squashfs' (default) or 'erofs'"
-  exit 1
-  ;;
-esac
-
-D="build/i12e-flatcar"
-DRAW="${D}.raw"
 
 set -e -o pipefail
-cd "$(dirname "$0")/.."
+
+function error_and_exit {
+  echo "error: $1" >&2
+  exit 1
+}
+
+DIST="dist"
+ROOTFS="${DIST}/rootfs"
+
+[ "$GITHUB_REPOSITORY" = "" ] && error_and_exit "undefined 'GITHUB_REPOSITORY'"
+REPO_NAME="${GITHUB_REPOSITORY#*/}"
+
+echo "GITHUB_REPOSITORY ... '${GITHUB_REPOSITORY}'"
+echo "REPO_NAME ........... '${REPO_NAME}'"
+
+FLATCAR_EXT="${REPO_NAME}-flatcar"
+FLATCAR_EXT_RAW="${FLATCAR_EXT}.raw"
+
 set -x
-rm -rf build
-umask 022
-mkdir -p $D/usr/bin $D/usr/lib/extension-release.d $D/usr/lib64
-go build -trimpath -buildvcs=false -ldflags="-s -w" -o build/i12e main.go
-cp build/i12e $D/usr/bin/i12e
-cp /usr/bin/{tmux,rclone} $D/usr/bin
-cp -a /usr/lib/x86_64-linux-gnu/libutempter.so* $D/usr/lib64
-cat <<__EOF >$D/usr/lib/extension-release.d/extension-release.i12e-flatcar
+
+rm -rf "${DIST}"
+go test -v ./...
+go build -trimpath -buildvcs=false -ldflags="-s -w" -o "${DIST}/${REPO_NAME}" main.go
+mkdir -p "${ROOTFS}/usr/bin" "${ROOTFS}/usr/lib/extension-release.d" "${ROOTFS}/usr/lib64"
+cp "${DIST}/${REPO_NAME}" "${ROOTFS}/usr/bin/${REPO_NAME}"
+cat <<__EOF >"${ROOTFS}/usr/lib/extension-release.d/extension-release.${FLATCAR_EXT}"
 ID=flatcar
 SYSEXT_LEVEL=1.0
 ARCHITECTURE=x86-64
 __EOF
-{ set +x; } 2>/dev/null
-case "$FS" in
-squashfs)
-  set -x
-  mksquashfs $D $DRAW -noappend -comp zstd -all-root
-  { set +x; } 2>/dev/null
-  ;;
-erofs)
-  set -x
-  mkfs.erofs --all-root -z lz4hc $DRAW $D
-  { set +x; } 2>/dev/null
-  ;;
-esac
-set -x
-ls -l $DRAW
-sha256sum $DRAW
+cp /usr/bin/tmux "${ROOTFS}/usr/bin/tmux"
+cp -a /usr/lib/x86_64-linux-gnu/libutempter.so* "${ROOTFS}/usr/lib64"
+mksquashfs "${ROOTFS}" "${DIST}/${FLATCAR_EXT_RAW}" -noappend -comp zstd -all-root
+
+(cd "${DIST}" && sha256sum "${FLATCAR_EXT_RAW}") >"${DIST}/${FLATCAR_EXT_RAW}.sha256"
