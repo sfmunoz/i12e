e45fdcad76b21660f5edf99ff6da85cc5df928e2
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-07 13:12:11 +0000

    build.sh: APP_NAME hardcoded (GITHUB_REPOSITORY based inference dropped)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,30 +2,19 @@
 
 set -e -o pipefail
 
-function error_and_exit {
-  echo "error: $1" >&2
-  exit 1
-}
-
 DIST="dist"
 ROOTFS="${DIST}/rootfs"
-
-[ "$GITHUB_REPOSITORY" = "" ] && error_and_exit "undefined 'GITHUB_REPOSITORY'"
-REPO_NAME="${GITHUB_REPOSITORY#*/}"
-
-echo "GITHUB_REPOSITORY ... '${GITHUB_REPOSITORY}'"
-echo "REPO_NAME ........... '${REPO_NAME}'"
-
-FLATCAR_EXT="${REPO_NAME}-flatcar"
+APP_NAME="i12e"
+FLATCAR_EXT="${APP_NAME}-flatcar"
 FLATCAR_EXT_RAW="${FLATCAR_EXT}.raw"
 
 set -x
 
 rm -rf "${DIST}"
 go test -v ./...
-go build -trimpath -buildvcs=false -ldflags="-s -w" -o "${DIST}/${REPO_NAME}" main.go
+go build -trimpath -buildvcs=false -ldflags="-s -w" -o "${DIST}/${APP_NAME}" main.go
 mkdir -p "${ROOTFS}/usr/bin" "${ROOTFS}/usr/lib/extension-release.d" "${ROOTFS}/usr/lib64"
-cp "${DIST}/${REPO_NAME}" "${ROOTFS}/usr/bin/${REPO_NAME}"
+cp "${DIST}/${APP_NAME}" "${ROOTFS}/usr/bin/${APP_NAME}"
 cat <<__EOF >"${ROOTFS}/usr/lib/extension-release.d/extension-release.${FLATCAR_EXT}"
 ID=flatcar
 SYSEXT_LEVEL=1.0
