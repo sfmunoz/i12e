699481173c670e4abc44c37d9d3c6648770de944
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-15 10:54:58 +0000

    i12e: '/etc/wireguard' mode set to '0700' instead of '0600'

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -61,7 +61,7 @@ func (a *Artifact) folders() error {
 		{Name: "etc/i12e/k3s", Mode: 0700},
 		{Name: "etc/systemd/system/k3s.service.d", Mode: 0755},
 		{Name: "etc/systemd/system.conf.d", Mode: 0755},
-		{Name: "etc/wireguard", Mode: 0600}, // it's ok and harmless: it already exists like this
+		{Name: "etc/wireguard", Mode: 0700}, // it's ok and harmless: it already exists like this
 		{Name: "opt/libexec", Mode: 0755},
 		{Name: "opt/libexec/i12e", Mode: 0755},
 	}
