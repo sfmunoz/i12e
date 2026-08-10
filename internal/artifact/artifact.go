5f3345f0f7fc14799eb27c10eddbce31d947c2cc
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-10 18:36:50 +0000

    artifact.go: 'etc/i12e/postgres-rclone/postgres-rclone.yaml' added

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -68,6 +68,7 @@ func (a *Artifact) folders() error {
 		{Name: "etc/i12e/csi-rclone", Mode: 0700},
 		{Name: "etc/i12e/traefik-config", Mode: 0700},
 		{Name: "etc/i12e/reflector", Mode: 0700},
+		{Name: "etc/i12e/postgres-rclone", Mode: 0700},
 		{Name: "etc/systemd/system/k3s.service.d", Mode: 0755},
 		{Name: "etc/systemd/system.conf.d", Mode: 0755},
 		{Name: "etc/wireguard", Mode: 0700}, // it's ok and harmless: it already exists like this
@@ -278,6 +279,13 @@ func (a *Artifact) etcI12eReflector() error {
 	return nil
 }
 
+func (a *Artifact) etcI12ePostgresRclone() error {
+	if err := a.addStatic("static/postgres-rclone.yaml", "etc/i12e/postgres-rclone/postgres-rclone.yaml", 0600); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (a *Artifact) etcNftablesConf() error {
 	data := struct {
 		PortKnocking []int
@@ -473,6 +481,7 @@ func (a *Artifact) run() error {
 		a.etcI12eCsiRclone,
 		a.etcI12eTraefikConfig,
 		a.etcI12eReflector,
+		a.etcI12ePostgresRclone,
 		a.etcNftablesConf,
 		a.etcSystemdSystemConfDI12eConf,
 		a.etcSystemdSystemK3sServiceDOverrideConf,
