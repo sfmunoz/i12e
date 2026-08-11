5a33686b2b3096cb5ecc6e25ded51bd875b1b953
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-11 16:40:35 +0000

    plugins: most of them adapted to use 'plugin-run.sh'

--- plugins/10-csi-rclone.sh
+++ plugins/10-csi-rclone.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-SRC="/etc/i12e/csi-rclone/csi-rclone.yaml"
-DST_DIR="/var/lib/rancher/k3s/server/manifests"
-DST="${DST_DIR}/csi-rclone.yaml"
-
-[ ! -f "${SRC}" ] && exit 0
-[ ! -d "${DST_DIR}" ] && exit 0
-
-if [ -f "${DST}" ]; then
-  (cd /etc/i12e/csi-rclone && sha256sum csi-rclone.yaml) | (cd /var/lib/rancher/k3s/server/manifests/ && sha256sum -c) >/dev/null 2>&1 && exit 0
-fi
-
-set -x -e
-install -o root -g root -m 0600 "${SRC}" "${DST}"
+cd "$(dirname "$0")"
+exec bash ../plugin-run.sh csi-rclone
--- plugins/20-reflector.sh
+++ plugins/20-reflector.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-SRC="/etc/i12e/reflector/reflector.yaml"
-DST_DIR="/var/lib/rancher/k3s/server/manifests"
-DST="${DST_DIR}/reflector.yaml"
-
-[ ! -f "${SRC}" ] && exit 0
-[ ! -d "${DST_DIR}" ] && exit 0
-
-if [ -f "${DST}" ]; then
-  (cd /etc/i12e/reflector && sha256sum reflector.yaml) | (cd /var/lib/rancher/k3s/server/manifests/ && sha256sum -c) >/dev/null 2>&1 && exit 0
-fi
-
-set -x -e
-install -o root -g root -m 0600 "${SRC}" "${DST}"
+cd "$(dirname "$0")"
+exec bash ../plugin-run.sh reflector
