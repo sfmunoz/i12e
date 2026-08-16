d6a1a84c33459b5f633fb5c58e1b4cfeaa9195de
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-16 16:48:02 +0000

    plugins/99-purge.sh: array used to define ENTRIES to allow spaces

--- plugins/99-purge.sh
+++ plugins/99-purge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-ENTRIES="/opt/libexec/i12e/plugins/02-namespaces.sh /etc/i12e/namespaces"
-for E in $ENTRIES; do
-  [ "$E" != "" ] || continue
+ENTRIES=(
+  "/opt/libexec/i12e/plugins/02-namespaces.sh"
+  "/etc/i12e/namespaces"
+)
+for E in "${ENTRIES[@]}"; do
   [ -e "$E" ] || continue
   set -x
   rm -rfv "$E"
