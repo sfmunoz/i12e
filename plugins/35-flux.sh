a7ea0725ef88dd65e323e40861678fab0d816777
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-16 05:54:50 +0000

    plugins/35-flux.sh: 'flux_install()' renamed to 'flux_bin_install()'

--- plugins/35-flux.sh
+++ plugins/35-flux.sh
@@ -3,7 +3,7 @@ export FLUX_VERSION="2.9.4" # exported: it's used by https://fluxcd.io/install.s
 SHA256SUM="0c91d4bbbc2aa9c84b42608184534437ef92e5f2d6b862e99a11c0bb24ad0941"
 FLUX_BIN="/opt/bin/flux"
 set -x -e -o pipefail
-function flux_install {
+function flux_bin_install {
   [ -x "${FLUX_BIN}" ] && [ "$(sha256sum "${FLUX_BIN}")" = "${SHA256SUM}  ${FLUX_BIN}" ] && return 0
   rm -rf "${FLUX_BIN}"
   curl -s https://fluxcd.io/install.sh | bash -s - /opt/bin
@@ -12,5 +12,5 @@ function flux_install {
   echo "error: '${FLUX_BIN}' download failed" >&2
   return 1
 }
-flux_install || exit $?
+flux_bin_install || exit $?
 exit 0
