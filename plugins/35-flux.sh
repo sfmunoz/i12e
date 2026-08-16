d422eb58643d77da002262abe72b55f0f894c447
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-16 09:54:24 +0000

    plugins/35-flux.sh: 'flux_cfg_read()' verifies 'AGE_KEY' is defined

--- plugins/35-flux.sh
+++ plugins/35-flux.sh
@@ -17,14 +17,15 @@ function flux_bin_install {
 }
 function flux_cfg_read {
   [ -f "$FLUX_CFG" ] || return 1
-  # set +x ... set -x: make sure GITHUB_TOKEN is not shown
+  # set +x ... set -x: make sure GITHUB_TOKEN and AGE_KEY are not shown
   { set +x; } 2>/dev/null
   echo "+ source $FLUX_CFG" >&2
-  source "$FLUX_CFG" # read GITHUB_TOKEN and CLUSTER
+  source "$FLUX_CFG" # read CLUSTER, GITHUB_TOKEN and AGE_KEY
   set -x
-  [ ${#GITHUB_TOKEN} -gt 0 ] || return 1 # length: GITHUB_TOKEN is not shown
-  export GITHUB_TOKEN
   [ "${CLUSTER}" = "dev" -o "${CLUSTER}" = "prod" ] || return 1
+  [ ${#GITHUB_TOKEN} -gt 0 ] || return 1 # length: GITHUB_TOKEN is not shown
+  [ ${#AGE_KEY} -gt 0 ] || return 1      # length: AGE_KEY is not shown
+  export CLUSTER GITHUB_TOKEN AGE_KEY
   return 0
 }
 function flux_bootstrap {
