c8d7cb692b3e74b84c3d3d4fd1e6108d74409bd6
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-16 05:49:37 +0000

    plugins/35-flux.sh: 'FLUX_BIN' verification reduced to one line

--- plugins/35-flux.sh
+++ plugins/35-flux.sh
@@ -4,9 +4,7 @@ SHA256SUM="0c91d4bbbc2aa9c84b42608184534437ef92e5f2d6b862e99a11c0bb24ad0941"
 FLUX_BIN="/opt/bin/flux"
 set -x -e -o pipefail
 function flux_install {
-  if [ -x "${FLUX_BIN}" ] && [ "$(sha256sum "${FLUX_BIN}")" = "${SHA256SUM}  ${FLUX_BIN}" ]; then
-    return 0
-  fi
+  [ -x "${FLUX_BIN}" ] && [ "$(sha256sum "${FLUX_BIN}")" = "${SHA256SUM}  ${FLUX_BIN}" ] && return 0
   rm -rf "${FLUX_BIN}"
   curl -s https://fluxcd.io/install.sh | bash -s - /opt/bin
   [ "$(sha256sum "${FLUX_BIN}")" = "${SHA256SUM}  ${FLUX_BIN}" ] && return 0
