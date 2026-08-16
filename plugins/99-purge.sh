ab384bb3354611afc3bc9bba9eddb6f77ded9fb5
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-08-16 16:52:39 +0000

    plugins/99-purge.sh: 'rclone-conf' plugin purged

--- plugins/99-purge.sh
+++ plugins/99-purge.sh
@@ -4,6 +4,7 @@ ENTRIES=(
   "/etc/i12e/traefik-config/"
   "/opt/libexec/i12e/plugins/02-namespaces.sh"
   "/opt/libexec/i12e/plugins/05-traefik-config.sh"
+  "/opt/libexec/i12e/plugins/08-rclone-conf.sh"
 )
 for E in "${ENTRIES[@]}"; do
   [ -e "$E" ] || continue
