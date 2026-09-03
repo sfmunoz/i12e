bb11f5a35d409d747b5fafa6d34a11df74c25d8e
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-03 15:41:43 +0000

    server: 'mesh_cfg.go' created and used

--- internal/server/server.go
+++ internal/server/server.go
@@ -51,6 +51,10 @@ func (s *Server) runOne(i int64) time.Duration {
 		log.Error("mesh.Run() failed", "err", err)
 		return s.slumber(0)
 	}
+	if err := meshCfg(s.cfg); err != nil {
+		log.Error("meshCfg() failed", "err", err)
+		return s.slumber(0)
+	}
 	if err := k3sInstall(s.cfg); err != nil {
 		log.Error("k3sInstall() failed", "err", err)
 		return s.slumber(0)
