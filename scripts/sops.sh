08a8f4089ee9fa8a2ace7046c33103ab31971c58
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-05 11:33:25 +0000

    scripts/sops.sh: simple 'sops decrypt ... | yq' wrapper created

---
+++ scripts/sops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+function error_and_exit {
+  echo "error: $1" >&2
+  exit 1
+}
+
+DNAME="$(dirname "$0")"
+
+[ "$I12E_SECRETS" = "" ] && I12E_SECRETS="${DNAME}/../../i12e-secrets"
+[ "$I12E_ENV" = "" ] && I12E_ENV="dev"
+
+export I12E_ENV
+
+BLOCK="$1"
+
+case "$BLOCK" in
+rclone)
+  sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/kube-system/rclone-conf.yaml" | yq -r .stringData.configData
+  exit $?
+  ;;
+restic)
+  APP="$2"
+  case "$APP" in
+  anki | forgejo | trilium)
+    sops decrypt ${I12E_SECRETS}/clusters/${I12E_ENV}/${APP}/restic-conf.yaml | yq -r .stringData.password
+    exit $?
+    ;;
+  *)
+    error_and_exit "unknown '${APP}' app"
+    ;;
+  esac
+  ;;
+*)
+  error_and_exit "unknown '${BLOCK}' block"
+  ;;
+esac
