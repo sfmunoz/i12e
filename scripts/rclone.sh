319e9f304095dd1099d82e76bc861aaa6b3ad019
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-05 11:37:02 +0000

    scripts/rclone.sh: adapted to use 'scripts/sops.sh' support

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -3,20 +3,14 @@
 set -e -o pipefail
 
 DNAME="$(dirname "$0")"
-
-[ "$I12E_SECRETS" = "" ] && I12E_SECRETS="${DNAME}/../../i12e-secrets"
-[ "$I12E_ENV" = "" ] && I12E_ENV="dev"
-
-export I12E_ENV
-
+SOPS="${DNAME}/sops.sh"
 TDIR="${XDG_RUNTIME_DIR}"
 
 [ "$TDIR" = "" ] && TDIR="/dev/shm"
 
 RCLONE_CONFIG="$(mktemp -p "$TDIR" rclone-$(id -u).XXXXXXXXXX.conf)"
 chmod 600 "${RCLONE_CONFIG}"
-sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/kube-system/rclone-conf.yaml" |
-  yq -r .stringData.configData >"$RCLONE_CONFIG"
+$SOPS rclone >"$RCLONE_CONFIG"
 
 exec 3<"$RCLONE_CONFIG"
 rm -f "$RCLONE_CONFIG"
