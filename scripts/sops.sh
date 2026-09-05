e42b143d40c25cb9475cebf3c9d8f251e26f9f26
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-05 17:00:28 +0000

    sops.sh: 'i12e-conf' 'artifact.port_knocking' processing generalized

--- scripts/sops.sh
+++ scripts/sops.sh
@@ -40,13 +40,10 @@ i12e-conf)
   ) | awk 'BEGIN { print "flux:" } { print " " $0 }'
   sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/i12e/butane.yaml" | yq -y '{ "butane": .stringData }'
   sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/i12e/artifact.yaml" |
-    yq -y '.stringData | {
-      "artifact": {
-        "port_knocking": .port_knocking | split(",") | map(tonumber),
-        "k3s_token": .k3s_token,
-        "k3s_agent_token": .k3s_agent_token,
-      }
-    }'
+    yq -y '.stringData |
+      to_entries |
+      map(if .key == "port_knocking" then .value |= (split(",") | map(tonumber)) else . end) |
+      from_entries'
   sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/i12e/mesh.yaml" | yq -y '{ "mesh": .stringData }'
   sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/i12e/wireguard.yaml" | yq -y '{ "wg_conf": .stringData.configData }'
   exit $?
