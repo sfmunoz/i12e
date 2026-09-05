95e51d5717d5c336c16d5af61b73a1733736d66b
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-05 10:01:17 +0000

    scripts/restic.sh: adapted to use 'scripts/rclone.sh'

--- scripts/restic.sh
+++ scripts/restic.sh
@@ -20,23 +20,15 @@ esac
 
 shift
 
-if [ "$I12E_SECRETS" = "" ]; then
-  cd "$(dirname "$0")"
-  I12E_SECRETS=../../i12e-secrets
-fi
+DNAME="$(dirname "$0")"
+
+[ "$I12E_SECRETS" = "" ] && I12E_SECRETS="${DNAME}/../../i12e-secrets"
 
 [ "$I12E_ENV" = "" ] && I12E_ENV="dev"
-
-export RCLONE_CONFIG="$(mktemp -p /dev/shm rclone-$(id -u).XXXXXXXXXX.conf)"
-trap "rm -vf '${RCLONE_CONFIG}'" EXIT
-chmod 600 "${RCLONE_CONFIG}"
-
-sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/kube-system/rclone-conf.yaml" |
-  yq -r .stringData.configData >"$RCLONE_CONFIG"
 
 export RESTIC_PASSWORD="$(sops decrypt ${I12E_SECRETS}/clusters/${I12E_ENV}/${APP}/restic-conf.yaml | yq -r .stringData.password)"
 export RESTIC_REPOSITORY="rclone:rem:${NAMESPACE}/${APP}"
 export RESTIC_CACHE_DIR=/dev/null
 
 set -x
-exec restic "$@"
+exec restic --option rclone.program="${DNAME}/rclone.sh" "$@"
