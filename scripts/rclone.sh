a21f976df59392163a302baed2aab51907ae2cac
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-05 09:22:35 +0000

    scripts/rclone.sh: simple rclone wrapper created

---
+++ scripts/rclone.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ "$I12E_SECRETS" = "" ]; then
+  cd "$(dirname "$0")"
+  I12E_SECRETS=../../i12e-secrets
+fi
+
+[ "$I12E_ENV" = "" ] && I12E_ENV="dev"
+
+export RCLONE_CONFIG="$(mktemp -p /dev/shm rclone-$(id -u).XXXXXXXXXX.conf)"
+trap "rm -vf '${RCLONE_CONFIG}'" EXIT
+chmod 600 "${RCLONE_CONFIG}"
+
+sops decrypt "${I12E_SECRETS}/clusters/${I12E_ENV}/kube-system/rclone-conf.yaml" |
+  yq -r .stringData.configData >"$RCLONE_CONFIG"
+
+set -x
+exec rclone "$@"
