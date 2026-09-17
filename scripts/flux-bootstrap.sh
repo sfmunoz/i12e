af7b84d582352c450e9ef516d59ce0013e8819ae
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-17 18:10:55 +0000

    scripts/flux-bootstrap.sh: 'CLUSTER' renamed to 'I12E_ENV'

--- scripts/flux-bootstrap.sh
+++ scripts/flux-bootstrap.sh
@@ -5,9 +5,9 @@ function error_and_exit {
   exit 1
 }
 
-CLUSTER="$1"
+I12E_ENV="$1"
 
-[ "${CLUSTER}" = "dev" -o "${CLUSTER}" = "prod" ] || error_and_exit "unknown cluster '${CLUSTER}' (valid: 'dev' or 'prod')"
+[ "${I12E_ENV}" = "dev" -o "${I12E_ENV}" = "prod" ] || error_and_exit "unknown I12E_ENV '${I12E_ENV}' (valid: 'dev' or 'prod')"
 
 set -x -e -o pipefail
 
@@ -15,10 +15,10 @@ flux bootstrap github \
   --token-auth \
   --owner=sfmunoz \
   --repository=i12e \
-  --path=clusters/${CLUSTER} \
+  --path=clusters/${I12E_ENV} \
   --branch=main \
   --private=false \
   --personal=true \
-  --author-name "flux-${CLUSTER}-bot" \
+  --author-name "flux-${I12E_ENV}-bot" \
   --author-email "46285520+sfmunoz@users.noreply.github.com" \
   --components-extra=source-watcher
