32afa25b5409e654dcddfd82dd4baa5f7ac36b5c
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-25 14:56:06 +0000

    plugins/00-purge.sh: '/etc/wireguard/wg0.conf' purged

--- plugins/00-purge.sh
+++ plugins/00-purge.sh
@@ -9,6 +9,7 @@ ENTRIES=(
   "/etc/i12e/wikijs"
   "/etc/i12e/anki-sync-server"
   "/etc/i12e/flux/sops-age.yaml"
+  "/etc/wireguard/wg0.yaml"
   "/opt/libexec/i12e/plugins/02-namespaces.sh"
   "/opt/libexec/i12e/plugins/05-traefik-config.sh"
   "/opt/libexec/i12e/plugins/08-rclone-conf.sh"
