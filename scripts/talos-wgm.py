7aa0fbcfd9dce20ea10a5cb2b7d1945acac7995b
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-09-25 15:08:05 +0000

    talos-wgm.py: 'wgi' renamed to 'wgm'

--- scripts/talos-wgm.py
+++ scripts/talos-wgm.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     ret = {
         "apiVersion": "v1alpha1",
         "kind": "WireguardConfig",
-        "name": "wgi",
+        "name": "wgm",
         "mtu": 1420,
         "up": True,
         "privateKey": node["key"],
